187f3838ac07fdb213360440c2bc10c7c82b2671
drsatan1 <simon.visser0@gmail.com>
2016-01-13 14:09:40 +0200

    Basic Jquery
    Adds navbar, links to each page. also document structure.

---
+++ src.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	var pathname = $(location).attr('href');
+	var last = pathname.substring(pathname.lastIndexOf("/") + 1, pathname.length);
+	
+	$('#menuJs').append('<li><a href="index.html" accesskey="1" title="">Homepage</a></li>');
+	$('#menuJs').append('<li><a href="about.html" accesskey="2" title="">About Us</a></li>');
+	$('#menuJs').append('<li><a href="solutions.html" accesskey="3" title="">Services & Solutions</a></li>');
+	$('#menuJs').append('<li><a href="careers.html" accesskey="4" title="">Careers</a></li>');
+
+	$('#menuJs li a').each(function(){
+		var here = this.href;
+		var thisLast = here.substring(here.lastIndexOf("/") + 1, here.length);
+		
+		if (thisLast == last){
+			$(this).parent().addClass("active");
+		}
+	});
+});
